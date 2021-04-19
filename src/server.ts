3be0b1df387244721c7c96861b0a5bd59a9933df
eliasall3x <elliasallex@gmail.com>
2021-04-19 19:46:09 -0300

    initial commit

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express from "express";
 
 import { routes } from "./routes";
@@ -6,6 +7,7 @@ const app = express();
 
 // MIDDLEWARES
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.listen(process.env.PORT || 3333, () =>
